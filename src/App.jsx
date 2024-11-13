2f023bfe3508ea06ad886d437ba7c4817b5a8593
zikniki <102323125+zikniki@users.noreply.github.com>
2024-11-13 19:53:45 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import react from 'react'
 import './App.css'
-import Organisms from './components/organismes/organisms'
+import Organisms from './components/organismes/Organisms'
+
 
 function App() {
 
   return (
     <div className='section'>
-      <Organisms />
+      <Organisms/>
     </div>
   )
 }
